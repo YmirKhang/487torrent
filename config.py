9cebb7d240f3f2bffe316de8ebee307deca52ac1
Emirhan Karagül <emirhan350z@gmail.com>
2018-11-29 21:02:20 +0300

    Add skeleton items

---
+++ config.py
@@ -0,0 +1,18 @@
+import socket
+
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        s.connect(('10.255.255.255', 1))
+        IP = s.getsockname()[0]
+    except:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
+
+SELF_IP = get_ip()
+DISCOVERY_PORT = 5000
+FILE_PORT = 5001
+ACK_PORT = 5002
+SUBNET = SELF_IP[:SELF_IP.rfind('.')]
